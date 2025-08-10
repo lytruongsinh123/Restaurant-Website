2ad516b31c72ac6cce4b15cfbbe3ce65a5ec5295
lytruongsinh123 <diepduong607@gmail.com>
2025-08-10 14:51:05 +0700

    change

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "antd";
+
+export default function Home() {
+  return (
+      <div>Hello World
+          <div>
+              <Button type="primary">Click Me</Button>
+          </div>
+    </div>
+  );
+}
