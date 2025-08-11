f95e279384bedb25f4f7118e64e5a680fcc47099
lytruongsinh123 <diepduong607@gmail.com>
2025-08-11 22:54:33 +0700

    change

--- nest-server/src/main.ts
+++ nest-server/src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    const configService = app.get(ConfigService);
-    const port = configService.get('PORT');
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  app.useGlobalPipes(
+    new ValidationPipe({
+        whitelist: true, // thuộc tính chỉ giữ lại các field thuộc DTO
+        forbidNonWhitelisted: true // thuộc tính không có trong DTO sẽ bị từ chối
+    }),
+  );
+  app.setGlobalPrefix('api/v1', { exclude: [''] });
   await app.listen(port);
 }
 bootstrap();
--- nest-server/src/modules/users/users.service.ts
+++ nest-server/src/modules/users/users.service.ts
@@ -1,15 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { hashPasswordHelper } from '@/utils/hashPasswordHelper';
+import aqp from 'api-query-params';
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+  ) {}
+  isEmailExist = async (email: string) => {
+    const user = await this.userModel.exists({ email });
+    if (user) return true;
+    return false;
+  };
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password, phone, address, image } = createUserDto;
+    //check email
+    const isUserExist = await this.isEmailExist(email);
+    if (isUserExist)
+      throw new BadRequestException(
+        `Email already exists : ${email}. Please choose another one.`,
+      );
+    //hash password
+    const hashPassword = await hashPasswordHelper(password);
+    const user = await this.userModel.create({
+      name,
+      email,
+      password: hashPassword,
+      phone,
+      address,
+      image,
+    });
+    return {
+      _id: user._id,
+    };
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll(query: string, current: number, pageSize: number) {
+    const { filter, sort } = aqp(query);
+    if (filter.current) delete filter.current;
+    if (filter.pageSize) delete filter.pageSize;
+    if (!current) current = 1;
+    if (!pageSize) pageSize = 10;
+    const totalItems = (await this.userModel.find(filter)).length;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    const offset = (current - 1) * pageSize;
+    const results = await this.userModel
+      .find(filter)
+      .limit(pageSize)
+      .skip(offset)
+      .select('-password') // Exclude password from results
+      .sort(sort as any);
+    return {
+      results,
+      totalPages,
+    };
   }
 
   findOne(id: number) {
